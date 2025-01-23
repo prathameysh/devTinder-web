11b35c4f0b7827f86909573883f5b8c36bbe2589
prathameysh <prathameshgavatre@gmail.com>
2025-01-23 12:08:54 +0530

    Validate Login and get Cookie

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,4 +1,61 @@
+import { useState } from "react";
+import axios from "axios";
+
+
 const Login = () => {
-  return <div>Login</div>;
+  const [emailId, setEmailId] = useState("Sabrina@gmail.com");
+  const [password, setPassword] = useState("Sabrina@123");
+  
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:7777/login",
+        { emailId, password },
+        { withCredentials: true } // Ensure credentials are included
+      );
+      console.log("Login successful:", res.data);
+    } catch (err) {
+      console.error("Login failed:", err);
+    }
+  };
+
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div>
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text">Email ID:</span>
+              </div>
+              <input
+                type="text"
+                value={emailId}
+                className="input input-bordered w-full max-w-xs"
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+            </label>
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text">Password</span>
+              </div>
+              <input
+                type="text"
+                value={password}
+                className="input input-bordered w-full max-w-xs"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+          </div>
+          <div className="card-actions justify-center m-2">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Login;
